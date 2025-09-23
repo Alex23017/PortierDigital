4be938e68c91bdf9c49d00c7b1f75d4433d520ce
Alex Yavorskyi <entertains1997@gmail.com>
2025-09-23 17:42:54 +0300

    home-pages

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,141 +1,155 @@
-import gulp from "gulp";
+// gulpfile.mjs
+import gulp, { src, dest } from "gulp";
 import fileInclude from "gulp-file-include";
 import gulpSass from "gulp-sass";
 import * as sass from "sass";
-const scss = gulpSass(sass);
 import autoprefixer from "gulp-autoprefixer";
 import browserSyncLib from "browser-sync";
-const browserSync = browserSyncLib.create();
 import clean from "gulp-clean";
 import fs from "fs";
 import sourcemaps from "gulp-sourcemaps";
 import plumber from "gulp-plumber";
 import notify from "gulp-notify";
-import imagemin from "gulp-imagemin";
-import changed from "gulp-changed";
+import imagemin from "gulp-imagemin"; // версия 4.x
 import postcss from "gulp-postcss";
 import pxtorem from "postcss-pxtorem";
 import webpackStream from "webpack-stream";
 import webpack from "webpack";
+import webp from "gulp-webp";
+import merge from "merge-stream";
+import mozjpeg from "imagemin-mozjpeg";
+import optipng from "imagemin-optipng";
+import svgo from "imagemin-svgo";
 import webpackConfig from "./webpack.config.js";
 
-const fileIncludeSettings = {
-  prefix: "@@",
-  basepath: "@file",
-};
+const scss = gulpSass(sass);
+const browserSync = browserSyncLib.create();
 
-gulp.task("js:docs", function (done) {
-  gulp
-    .src("./src/js/**/*.js")
-    .pipe(plumber(plumberConfig("JS")))
-    .pipe(webpackStream(webpackConfig, webpack))
-    .pipe(gulp.dest("./docs/js"))
-    .on("end", function () {
-      browserSync.reload();
-      done();
-    });
+// ----------------------------------------
+// Plumber Error Handler
+// ----------------------------------------
+const plumberConfig = (title) => ({
+  errorHandler: notify.onError({
+    title,
+    message: "Error <%= error.message %>",
+    sound: false,
+  }),
 });
 
-const plumberConfig = (title) => {
-  return {
-    errorHandler: notify.onError({
-      title: title,
-      message: "Error <%= error.message %>",
-      sound: false,
-    }),
-  };
-};
-
-gulp.task("html:docs", function () {
-  return gulp
-    .src("./src/pages/**/*.html")
+// ----------------------------------------
+// HTML
+// ----------------------------------------
+export const htmlDocs = () =>
+  src("./src/pages/**/*.html")
     .pipe(plumber(plumberConfig("HTML")))
-    .pipe(fileInclude(fileIncludeSettings))
-    .pipe(gulp.dest("./docs/"))
+    .pipe(fileInclude({ prefix: "@@", basepath: "@file" }))
+    .pipe(dest("./docs/"))
     .pipe(browserSync.stream());
-});
 
-gulp.task("components:docs", function () {
-  return gulp
-    .src("./src/components/**/*.html")
-    .pipe(plumber(plumberConfig("components")))
-    .pipe(fileInclude(fileIncludeSettings))
-    .pipe(gulp.dest("./docs/components/"))
+// ----------------------------------------
+// Components
+// ----------------------------------------
+export const componentsDocs = () =>
+  src("./src/components/**/*.html")
+    .pipe(plumber(plumberConfig("Components")))
+    .pipe(fileInclude({ prefix: "@@", basepath: "@file" }))
+    .pipe(dest("./docs/components/"))
     .pipe(browserSync.stream());
-});
 
-gulp.task("scss:docs", function () {
-  return gulp
-    .src("./src/scss/**/*.scss")
+// ----------------------------------------
+// SCSS
+// ----------------------------------------
+export const scssDocs = () =>
+  src("./src/scss/**/*.scss")
     .pipe(plumber(plumberConfig("STYLES")))
     .pipe(sourcemaps.init())
     .pipe(scss())
-    .pipe(
-      postcss([
-        pxtorem({
-          rootValue: 16,
-          propList: ["*"],
-          replace: true,
-        }),
-      ]),
-    )
+    .pipe(postcss([pxtorem({ rootValue: 16, propList: ["*"], replace: true })]))
     .pipe(autoprefixer())
     .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("./docs/css/"))
+    .pipe(dest("./docs/css/"))
     .pipe(browserSync.stream());
-});
 
-gulp.task("images:docs", function () {
-  return gulp
-    .src("./src/img/**/*.{png,jpg,jpeg,gif,svg}")
-    .pipe(changed("./docs/img/"))
-    .pipe(imagemin({ verbose: true }))
-    .pipe(gulp.dest("./docs/img/"))
+// ----------------------------------------
+// JS
+// ----------------------------------------
+export const jsDocs = (done) => {
+  src("./src/js/**/*.js")
+    .pipe(plumber(plumberConfig("JS")))
+    .pipe(webpackStream(webpackConfig, webpack))
+    .pipe(dest("./docs/js"))
+    .on("end", () => {
+      browserSync.reload();
+      done();
+    });
+};
+
+// ----------------------------------------
+// Images (gulp-imagemin v4 + WebP)
+// ----------------------------------------
+export const imagesDocs = () => {
+  const optimized = src("./src/img/**/*.*", { encoding: false })
+    .pipe(
+      imagemin([
+        mozjpeg({ progressive: true, quality: 75 }),
+        optipng({ optimizationLevel: 5 }),
+        svgo({ plugins: [{ name: "removeViewBox", active: false }] }),
+      ])
+    )
+    .pipe(dest("./docs/img/"));
+
+  const webpImages = src("./src/img/**/*.{png,jpg,jpeg}", { encoding: false })
+    .pipe(webp({ quality: 70 }))
+    .pipe(dest("./docs/img/"));
+
+  return merge(optimized, webpImages).on("end", () => browserSync.reload());
+};
+
+// ----------------------------------------
+// Fonts
+// ----------------------------------------
+export const fontsDocs = () =>
+  src("./src/fonts/**/*")
+    .pipe(dest("./docs/fonts/"))
     .pipe(browserSync.stream());
-});
-gulp.task("fonts:docs", function () {
-  return gulp.src("./src/fonts/**/*").pipe(gulp.dest("./docs/fonts/")).pipe(browserSync.stream());
-});
 
-gulp.task("clean:docs", function (done) {
+// ----------------------------------------
+// Clean
+// ----------------------------------------
+export const cleanDocs = (done) => {
   if (fs.existsSync("./docs/")) {
-    return gulp.src("./docs/", { read: false }).pipe(clean({ force: true }));
+    return src("./docs/", { read: false }).pipe(clean({ force: true }));
   }
   done();
-});
+};
 
-gulp.task("server:docs", function () {
+// ----------------------------------------
+// Server
+// ----------------------------------------
+export const serverDocs = () =>
   browserSync.init({
-    server: {
-      baseDir: "./docs",
-      index: "index.html",
-    },
+    server: { baseDir: "./docs", index: "index.html" },
     port: 8080,
     open: true,
   });
-});
 
-gulp.task("watch:docs", function () {
-  gulp.watch("./src/scss/**/*.scss", gulp.series("scss:docs"));
-  gulp.watch("./src/components", gulp.series("components:docs"));
-  gulp.watch("./src/**/*.html", gulp.series("html:docs"));
-  gulp.watch("./src/img/**/*", gulp.series("images:docs"));
-  gulp.watch("./src/js/**/*.js", gulp.series("js:docs"));
-  gulp.watch("./src/fonts/**/*", gulp.series("fonts:docs"));
-});
+// ----------------------------------------
+// Watch
+// ----------------------------------------
+export const watchDocs = () => {
+  gulp.watch("./src/scss/**/*.scss", scssDocs);
+  gulp.watch("./src/components/**/*.html", componentsDocs);
+  gulp.watch("./src/pages/**/*.html", htmlDocs);
+  gulp.watch("./src/img/**/*.{png,jpg,jpeg,gif,svg}", imagesDocs);
+  gulp.watch("./src/js/**/*.js", jsDocs);
+  gulp.watch("./src/fonts/**/*", fontsDocs);
+};
 
-gulp.task(
-  "default",
-  gulp.series(
-    "clean:docs",
-    gulp.parallel(
-      "html:docs",
-      "scss:docs",
-      "images:docs",
-      "js:docs",
-      "fonts:docs",
-      "components:docs",
-    ),
-    gulp.parallel("server:docs", "watch:docs"),
-  ),
+// ----------------------------------------
+// Default
+// ----------------------------------------
+export default gulp.series(
+  cleanDocs,
+  gulp.parallel(htmlDocs, scssDocs, imagesDocs, jsDocs, fontsDocs, componentsDocs),
+  gulp.parallel(serverDocs, watchDocs)
 );
