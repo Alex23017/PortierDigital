a052a68a48d2b80afd35d1ecbe8c83d0d899f608
Alex Yavorskyi <entertains1997@gmail.com>
2025-09-24 16:04:46 +0300

    arrowUp

--- src/js/cardsPortfolio.js
+++ src/js/cardsPortfolio.js
@@ -1,33 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
   const arrayCards = [
     {
-      img: "../img/porftolioPage/cards/iconSmile.webp",
+      img: "img/porftolioPage/cards/iconSmile.webp",
       title: "Passionate",
-      text: "As Pasteur himself commented, “Chance favors",
+      text: "As Pasteur himself commented, “Chance favors only the prepared mind.",
     },
     {
-      img: "../img/porftolioPage/cards/iconLup.webp",
+      img: "img/porftolioPage/cards/iconLup.webp",
       title: "Curious",
       text: "As Pasteur himself commented, “Chance favors only the prepared mind.",
     },
     {
-      img: "../img/porftolioPage/cards/iconPlanet.webp",
+      img: "img/porftolioPage/cards/iconPlanet.webp",
       title: "Team Work",
       text: "As Pasteur himself commented, “Chance favors only the prepared mind.",
     },
     {
-      img: "../img/porftolioPage/cards/iconFigure.webp",
+      img: "img/porftolioPage/cards/iconFigure.webp",
       title: "Discipline",
       text: "As Pasteur himself commented, “Chance favors only the prepared mind.",
       color: "#57efb4",
     },
     {
-      img: "../img/porftolioPage/cards/iconStick.webp",
+      img: "img/porftolioPage/cards/iconStick.webp",
       title: "Creative",
       text: "As Pasteur himself commented, “Chance favors only the prepared mind.",
     },
     {
-      img: "../img/porftolioPage/cards/iconMan.webp",
+      img: "img/porftolioPage/cards/iconMan.webp",
       title: "Compassionate",
       text: "As Pasteur himself commented, “Chance favors only the prepared mind.",
     },
